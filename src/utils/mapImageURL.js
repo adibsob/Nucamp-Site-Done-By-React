40130d28c0306c288f53e72a31dcaa1afd737cdc
adibsob <adibsobhanian@yahoo.com>
2022-10-21 21:58:57 -0700

    Async Campsites

---
+++ src/utils/mapImageURL.js
@@ -0,0 +1,10 @@
+import { baseUrl } from "../app/shared/basedUrl";
+
+export const mapImageURL = (arr) => {
+    return arr.map((item) => {
+        return {
+            ...item,
+            image: baseUrl + item.image
+        };
+    });
+};
